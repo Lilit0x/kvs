2171cf2a16c081dd5a29c9e3bac75d552d812bfd
Baasit <baasisek01@gmail.com>
2025-05-20 22:40:34 +0100

    feat(app): first project completed

--- src/bin/kvs.rs
+++ src/bin/kvs.rs
@@ -1,4 +1,50 @@
+use std::process::exit;
+
+use clap::{Parser, Subcommand};
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Cli {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Debug, Subcommand)]
+enum Commands {
+    /// Set the value of a string key to a string
+    Set {
+        /// key
+        key: String,
+
+        /// value
+        value: String,
+    },
+    /// Get the string value of a given string key
+    Get {
+        /// key to get
+        key: String,
+    },
+    /// Remove a given key
+    Rm {
+        /// key to remove
+        key: String,
+    },
+}
+
 fn main() {
-    
-    println!("Hello, world!");
+    let cli = Cli::parse();
+    match &cli.command {
+        Commands::Set { key, value } => {
+            eprintln!("unimplemented");
+            exit(1);
+        }
+        Commands::Get { key } => {
+            eprintln!("unimplemented");
+            exit(1);
+        }
+        Commands::Rm { key } => {
+            eprintln!("unimplemented");
+            exit(1);
+        }
+    }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,19 +1,63 @@
-pub struct KvStore {}
+#![deny(missing_docs)]
+//! A simple in-memory key/value store that maps strings to strings
+use std::collections::HashMap;
+
+/// The main store
+pub struct KvStore {
+    store: HashMap<String, String>,
+}
+
+impl Default for KvStore {
+    fn default() -> Self {
+        Self::new()
+    }
+}
 
 impl KvStore {
+    /// Initialize a new insyance of the store
+    /// ```rust
+    /// use kvs::KvStore;
+    /// let mut store = KvStore::new();
+    /// ```
     pub fn new() -> Self {
-        unimplemented!()
+        Self {
+            store: HashMap::new(),
+        }
     }
 
+    /// Set the value of a string key to a string
+    /// ```rust
+    /// use kvs::KvStore;
+    /// let mut store = KvStore::new();
+    /// store.set("key1".to_owned(), "value1".to_owned());
+    /// assert_eq!(store.get("key1".to_owned()), Some("value1".to_owned()));
+    /// ```
     pub fn set(&mut self, key: String, value: String) {
-        unimplemented!()
+        self.store.insert(key, value);
     }
 
+    /// Get the string value of a given string key
+    /// ```rust
+    /// use kvs::KvStore;
+    /// let mut store = KvStore::new();
+    /// store.set("key1".to_owned(), "value1".to_owned());
+    /// store.set("key2".to_owned(), "value2".to_owned());
+    /// assert_eq!(store.get("key1".to_owned()), Some("value1".to_owned()));
+    /// assert_eq!(store.get("key2".to_owned()), Some("value2".to_owned()));
+    /// ```
     pub fn get(&self, key: String) -> Option<String> {
-        unimplemented!()
+        self.store.get(&key).cloned()
     }
 
+    /// Remove a given key
+    /// ```rust
+    /// use kvs::KvStore;
+    /// let mut store = KvStore::new();
+    ///  store.set("key1".to_owned(), "value1".to_owned());
+    /// store.remove("key1".to_owned());
+    /// assert_eq!(store.get("key1".to_owned()), None);
+    /// ```
     pub fn remove(&mut self, key: String) {
-        unimplemented!()
+        self.store.remove(&key);
     }
 }
